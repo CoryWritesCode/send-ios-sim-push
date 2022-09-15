3a2b1342203a305f72ceebf6715dcb8e7eacc482
Cory Cunningham <Corycunningham2010@gmail.com>
2022-09-15 15:32:30 -0400

    ✅ FEAT: initial commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+const { exec } = require("child_process");
+
+export type Message = {}
+
+const defaultMessage: Message = {
+  aps: {
+    alert: {
+      title: "Test",
+      body: "This is a default message from the package source.",
+    },
+  },
+};
+
+function sendSimulatorPushNotification(bundleId: string, message: Message) {
+  exec(
+    `echo '${JSON.stringify(
+      message ?? defaultMessage
+    )}' | xcrun simctl push booted ${bundleId} -`,
+    function (error) {
+      `echo ERROR: ${JSON.stringify(error)}`;
+    }
+  );
+}
+
+module.exports = sendSimulatorPushNotification
