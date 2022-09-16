f6b1f1a82c189cd5ce512c1bd981194d95778172
Cory Cunningham <Corycunningham2010@gmail.com>
2022-09-16 11:06:25 -0400

    👌 IMPROVE: prettier

--- index.ts
+++ index.ts
@@ -1,32 +1,32 @@
-import { exec } from "child_process";
+import { exec } from 'child_process';
 
 export type Message = {
   aps: {
-    alert: {
-      title: string;
-      body: string;
-    } | string;
+    alert:
+      | {
+          title: string;
+          body: string;
+        }
+      | string;
     badge?: number;
-    sound?: "Default";
-  }
-}
+    sound?: 'Default';
+  };
+};
 
 const defaultMessage: Message = {
   aps: {
     alert: {
-      title: "Test",
-      body: "This is a default message from the package source.",
+      title: 'Test',
+      body: 'This is a default message from the package source.',
     },
   },
 };
 
 export function sendSimulatorPushNotification(bundleId: string, message?: Message) {
   exec(
-    `echo '${JSON.stringify(
-      message ?? defaultMessage
-    )}' | xcrun simctl push booted ${bundleId} -`,
-    (error) => `echo ERROR: ${JSON.stringify(error)}`
+    `echo '${JSON.stringify(message ?? defaultMessage)}' | xcrun simctl push booted ${bundleId} -`,
+    (error) => `echo ERROR: ${JSON.stringify(error)}`,
   );
 }
 
-module.exports = sendSimulatorPushNotification
+module.exports = sendSimulatorPushNotification;
